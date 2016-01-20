608d76fc1efdc991982155294e0e838e2a073a69
Ivo List <ivo.list@guest.arnes.si>
2016-01-21 00:08:52 +0100

    Moving special.hs back to MPFRLowLevel.hs

--- src/Data/Approximate/MPFRLowLevel.hs
+++ src/Data/Approximate/MPFRLowLevel.hs
@@ -32,7 +32,6 @@ import GHC.Prim -- Int#, ByteArray#,
 import GHC.Types -- Word
 import GHC.Integer.GMP.Prim (int2Integer#)
 import Data.Approximate.MPFR.Types
-import Data.Approximate.MPFR.Special
 
 {- 5.2 Assignment Functions -}
 
@@ -370,6 +369,34 @@ isNumber :: Rounded -> Bool
 sgn :: Rounded -> Maybe Int
 
 --}
+
+{- 5.7 Special Functions -}
+
+#include "MPFR/special.h"
+
+sincos = sin_cos
+sincosh = sinh_cosh
+pi=const_pi
+euler=const_euler
+log2c=const_log2
+catalan=const_catalan
+
+
+{-
+facw :: RoundMode -> Precision -> GHC.Types.Word -> Rounded
+facw_ :: RoundMode -> Precision -> GHC.Types.Word -> (Rounded, Int)
+
+zetaw :: RoundMode -> Precision -> GHC.Types.Word -> Rounded
+zetaw_ :: RoundMode -> Precision -> GHC.Types.Word -> (Rounded, Int)
+
+jn :: RoundMode -> Precision -> Int -> Rounded -> Rounded
+jn_ :: RoundMode -> Precision -> Int -> Rounded -> (Rounded, Int)
+
+yn :: RoundMode -> Precision -> Int -> Rounded -> Rounded
+yn_ :: RoundMode -> Precision -> Int -> Rounded -> (Rounded, Int)
+
+-}
+
 
 
 {- 5.10 Integer and Remainder related functions -}
